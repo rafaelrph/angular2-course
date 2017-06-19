e5ee911415f01401fe4907bf1bc1a00ed1da479c
Rafael Barros <rafael.barros@mprn.mp.br>
2017-06-19 13:43:09 -0300

    Two-way data binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -9,11 +9,31 @@ export class DataBindingComponent implements OnInit {
 
   url: string = "http://github.com";
   urlImage: string = "http://lorempixel.com/400/200/nature/";
+  value: string = "";
+  valueSaved: string = "";
+  isMouseOver: boolean = false;
+  textValue:string = "text";
 
   constructor() { }
 
   getValue(){
     return 5;
+  }
+
+  clickHere() {
+    alert("Button was clicked");
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  onKeyUpInput(event: KeyboardEvent){
+    this.value = (<HTMLInputElement>event.target).value;
+  }
+
+  saveTextEnter(event: KeyboardEvent) {
+    this.valueSaved = (<HTMLInputElement>event.target).value;
   }
 
   ngOnInit() {
